01af6c17dc2f6af3b7c057998995667aaa90859e
robertpohl <robban.p@gmail.com>
2024-01-13 23:57:01 +0100

    handler

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -4,5 +4,10 @@ use axum::Json;
 use sqlx::{Pool, Postgres};
 
 pub async fn root_get(Extension(db): Extension<Pool<Postgres>>) -> Response {
-    return (StatusCode::OK, Json("hi")).into_response();
+    let merchant_count: Result<i64, sqlx::Error> = sqlx::query_scalar(
+        "select count(*) from merchants")
+    .fetch_one(&db)
+    .await;
+    
+    return (StatusCode::OK, Json(format!("found {:?} merchants", merchant_count.unwrap()))).into_response();
 }
