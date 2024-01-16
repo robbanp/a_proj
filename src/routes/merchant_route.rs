25792a5380c226ea684f28d3107c1ab61f40e410
robertpohl <robban.p@gmail.com>
2024-01-16 23:01:39 +0100

    get route

--- src/routes/merchant_route.rs
+++ src/routes/merchant_route.rs
@@ -86,3 +86,26 @@ pub async fn merchant_post(
           }
         }
     
+    pub async fn merchant_get(
+        Path(id): Path<i32>,
+        Extension(db): Extension<Pool<Postgres>>
+    ) ->  Response<Body>  {
+    
+        let result = sqlx::query_as!(Merchant,
+            r#"SELECT id, name, created_at, updated_at, metadata, status as "status!: enums::Status" FROM merchants
+            where id = $1"#,
+            id
+        )
+        .fetch_one(&db)
+        .await;
+    
+        match result {
+            Ok(merchants) =>  return (StatusCode::OK, Json(merchants)).into_response(),    
+            Err(_err) => {
+                dbg!(&_err);
+                let error = _err.as_database_error().map(|m| m.message());
+                return (StatusCode::UNPROCESSABLE_ENTITY, Json(json!(error))).into_response()
+            } 
+            }
+        }
+        
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -8,7 +8,7 @@ mod root;
 use root::root_get;
 
 mod merchant_route;
-use self::merchant_route::{merchant_post, merchant_list, merchant_update};
+use self::merchant_route::{merchant_post, merchant_list, merchant_update, merchant_get};
 
 pub fn create_routes(db: Pool<Postgres>) -> Router {
     let cors = CorsLayer::new()
@@ -20,6 +20,7 @@ pub fn create_routes(db: Pool<Postgres>) -> Router {
     .route("/merchants", post(merchant_post))
     .route("/merchants", get(merchant_list))
     .route("/merchants/:id", put(merchant_update))
+    .route("/merchants/:id", get(merchant_get))
     .layer(cors) // affect allroutes above
     .layer(Extension(db))
 }
